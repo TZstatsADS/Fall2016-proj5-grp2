502315286c7535efc6f4dc105721d6e6b188414f
Max Liu <zl2480@columbia.edu>
2016-12-04 23:47:15 -0500

    Create ui.R

---
+++ lib/Shiny/ui.R
@@ -0,0 +1,42 @@
+library(shiny)
+library(shinythemes)
+library(networkD3)
+library(shinyjs)
+library(highcharter)
+
+tagList(
+  useShinyjs(),
+navbarPage('Songs!', theme = shinytheme('united'),
+           
+  tabPanel("Make your own song!",
+           sidebarLayout(
+             sidebarPanel(
+                radioButtons('Genre', 'Choose a Genre!', 
+                                   choices = c('R&B/Soul','Rock','Pop','Country', 'Others','Hip-hop', 'Folk', 'Jazz')),
+                
+                selectInput('nextPitch', 'What next?', choices = list('a','b','c','d'), selected = 'a'),
+                
+                actionButton('start', 'click to run')
+                
+                ),
+                mainPanel(
+                h2('Make your own song!'),
+                
+                simpleNetworkOutput("qiu"),
+                
+                highchartOutput('bar'),
+                
+                h4('Your song:'),
+                
+                textOutput('text'),
+                
+                hr(),
+                
+                #future change into output$html_text
+                #HTML('<h1>Music Player</h1><audio controls><source src="/Users/Max/Desktop/record.mp3" type="audio/mpeg"></source></audio>')
+                uiOutput('html')
+                #includeHTML('music_player.html')
+                )
+           ))
+  
+))
