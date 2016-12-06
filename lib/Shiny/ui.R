06f0944266512896cda346a71eb351d55fc5b693
Wanyi Zhang <mushroomvv@gmail.com>
2016-12-05 21:06:26 -0500

    Updated Shiny

--- lib/Shiny/ui.R
+++ lib/Shiny/ui.R
@@ -6,26 +6,26 @@ library(highcharter)
 
 tagList(
   useShinyjs(),
-navbarPage(strong('MelodySoup', style = "font-family: 'times'; font-size:16pt;"), theme = shinytheme('cosmo'),
+navbarPage(strong('MelodySoup', style = "font-family: 'times'; font-size:18pt;"), theme = shinytheme('cosmo'),
   # 1. Intro tab
   tabPanel("About Us", 
-          h1("MelodySoup - the base for writing melodies", style = "font-family: 'times'; font-size:16pt;"),
+          h1("MelodySoup - the base for writing melodies"),
           img(src='logo2.jpg', width="20%", align = "left"),
-          h2("How to Use MelodySoup"),
-          p("In 'Make your own song' tab, you can choose a genre you like,"),
-          p("pick your starting chords, and hit 'start a new song'."), 
-          p("Then you can choose next chord from top hot chord list we recommend for you."),
-          h2("Mission"),
-          p("We want to empower everyone to write simple but elegant music"),
+          h2(icon("check-circle"), "Our Mission"),
+          p("Anyone can write music.", style = "font-size:14pt"),
+          h2(icon("question-circle"), "How to Use MelodySoup"),
+          p("  1. In 'Make Your Own Song' tab, you can choose a genre you like.", style = "font-size:14pt"),
+          p("  2. Pick your starting chords, and hit 'start a new song'.", style = "font-size:14pt"), 
+          p("  3. Then you can choose next chord from top hot chord list we recommend for you.", style = "font-size:14pt"),
           img(src='Background4.jpg', width="120%", align = "right")
   ),
 
   # 2. Songmaking tab
-  tabPanel("Make your own song!",
+  tabPanel("Make Your Own Song!",
            sidebarLayout(
              sidebarPanel(
                 radioButtons('Genre', 'Choose a Genre!', 
-                                   choices = c('R&B/Soul','Rock','Pop','Country', 'Others','Hip-hop', 'Folk', 'Jazz')),
+                                   choices = c('All genres', 'R&B/Soul','Rock','Pop','Country', 'Others','Hip-hop', 'Folk', 'Jazz')),
                 
                 selectInput('nextPitch', 'What next?', choices = list('a','b','c','d'), selected = 'a'),
                 
