73317612219ddf3996880f05d25f98bb0e76d138
Wanyi Zhang <mushroomvv@gmail.com>
2016-12-06 17:19:33 -0500

    Updated Shiny

--- lib/Shiny/ui.R
+++ lib/Shiny/ui.R
@@ -18,7 +18,7 @@ navbarPage(strong('MelodySoup', style = "font-family: 'times'; font-size:18pt;")
           p("  2. Pick your starting chords, and hit 'start a new song'.", style = "font-size:14pt"), 
           p("  3. Then you can choose next chord from top hot chord list we recommend for you.", style = "font-size:14pt"),
           h3(icon("github"), "GitHub Link"),
-          p("https://github.com/TZstatsADS/Fall2016-proj5-grp2"),
+          a("https://github.com/TZstatsADS/Fall2016-proj5-grp2", href="https://github.com/TZstatsADS/Fall2016-proj5-grp2"),
           img(src='Background5.jpg', width="120%", align = "right")
   ),
 
@@ -51,6 +51,8 @@ navbarPage(strong('MelodySoup', style = "font-family: 'times'; font-size:18pt;")
                 uiOutput('html')
                 #includeHTML('music_player.html')
                 )
-           ))
+           ),
+           img(src='Background5.jpg', width="120%", align = "right")
+  )
   
 ))
